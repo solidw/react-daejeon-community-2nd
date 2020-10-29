e52f5555a75cba573eeacf5c6f2cf08f4e97afa3
solidw <solidw@naver.com>
2020-10-29 19:43:12 +0900

    feat: Fix Home.js for fetchData

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,36 +5,41 @@ import './Home.css';
 import RestaurantTable from '../components/RestaurantTable';
 import RandomPicker from '../components/RandomPicker';
 import Loading from '../components/Loading';
+import FetchButton from '../components/FetchButton';
 
 function Home() {
   const [restaurantData, setRestaurantData] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const fetchedData = await axios.get('http://localhost:4000/restaurant');
+      setRestaurantData(fetchedData.data.data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const fetchedData = await axios.get('http://localhost:4000/restaurant');
-        setRestaurantData(fetchedData.data.data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    };
     fetchData();
   }, []);
 
   if (isLoading) {
-    return <Loading />;
+    return <Loading center />;
   }
 
-  if (error != null) {
+  if (error) {
     return (
       <div>
-        error:
-        {' '}
-        {error}
+        <p>
+          Error
+        </p>
+        <FetchButton callback={fetchData} />
       </div>
     );
   }
