069a2e45fbb8af4a42dc992b5fc5006144b86ff8
solidw <solidw@naver.com>
2020-10-29 19:47:16 +0900

    feat: Override commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 
+import useAsync from '../hooks/useAsync';
 import './Home.css';
 import RestaurantTable from '../components/RestaurantTable';
 import RandomPicker from '../components/RandomPicker';
 import Loading from '../components/Loading';
+import FetchButton from '../components/FetchButton';
+
+async function getRestaurant() {
+  const response = await axios.get('http://localhost:4000/restaurant');
+  return response.data;
+}
 
 function Home() {
-  const [restaurantData, setRestaurantData] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const {
+    loading, data, error, refetch,
+  } = useAsync(getRestaurant, [], false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const fetchedData = await axios.get('http://localhost:4000/restaurant');
-        setRestaurantData(fetchedData.data.data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
+  if (loading) return <Loading center />;
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error != null) {
+  if (error) {
     return (
       <div>
-        error:
-        {' '}
-        {error}
+        <p>
+          Error
+        </p>
+        <FetchButton callback={refetch} />
       </div>
     );
   }
+
+  if (!data) return <div>No data</div>;
+
+  const { data: restaurantData } = data;
 
   return (
     <div className="home-div">
